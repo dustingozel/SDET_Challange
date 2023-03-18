552a71c39717141b39bc7ce79c788762b77622bf
gozelihsan <gozelihsan@gmail.com>
2023-03-18 02:26:30 -0400

    Added tests for locked out user

--- tests/pageObjects/loginPage.js
+++ tests/pageObjects/loginPage.js
@@ -2,7 +2,11 @@ class LoginPage {
 
     constructor(page) {
         this.page = page;
+        this.errorMessage = page.locator('//h3[@data-test="error"]');
+        this.errorButton = page.locator('//button[@class="error-button"]');
         this.logInButton = page.locator('//input[@id="login-button"]');
+        this.username = page.locator('//input[@id="user-name"]');
+        this.lastname = page.locator('//input[@id="password"]');
     }
 }
 
---
+++ tests/specs/lockedOutUser.spec.js
@@ -0,0 +1,64 @@
+const { test, expect } = require('@playwright/test');
+const { LoginPage } = require('../pageObjects/loginPage');
+
+const username = JSON.parse(JSON.stringify(require('../testData/userNames.json')));
+
+let page;
+
+test.beforeEach(async ({ browser }) => {
+  const context = await browser.newContext();
+  page = await context.newPage();
+  
+  await page.goto('https://www.saucedemo.com/');
+
+  const title = 'Swag Labs';
+  await expect(page).toHaveTitle(title);
+});
+
+test('Login Validation', async () => {
+  const login = new LoginPage(page);
+
+  await login.username.type(await username.lockedOutUser);
+  await login.lastname.type('secret_sauce');
+
+  expect(await login.logInButton).toBeVisible();
+
+  await login.logInButton.click();
+
+  expect(await login.errorMessage.textContent()).toEqual('Epic sadface: Sorry, this user has been locked out.');
+  expect(await login.errorButton).toBeVisible();
+});
+
+test('Login Validation by Sending Empty Username', async () => {
+    const login = new LoginPage(page);
+  
+    await login.username.type('');
+    await login.lastname.type('secret_sauce');
+  
+    await login.logInButton.click();
+  
+    expect(await login.errorMessage.textContent()).toEqual('Epic sadface: Username is required');
+});
+
+test('Login Validation by Sending Empty Password', async () => {
+    const login = new LoginPage(page);
+  
+    await login.username.type(await username.lockedOutUser);
+    await login.lastname.type('');
+  
+    await login.logInButton.click();
+  
+    expect(await login.errorMessage.textContent()).toEqual('Epic sadface: Password is required');
+});
+
+test('Login Validation by Sending Empty Username and Password', async () => {
+    const login = new LoginPage(page);
+  
+    await login.username.type('');
+    await login.lastname.type('');
+  
+    await login.logInButton.click();
+  
+    expect(await login.errorMessage.textContent()).toEqual('Epic sadface: Username is required');
+});
+
